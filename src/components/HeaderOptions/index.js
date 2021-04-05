d41e8766469085f5a7d064d58e9c26ed36bcbf98
JP <jpc.araujo@hotmail.com>
2021-04-05 01:01:22 -0300

    add onClick function

--- src/components/HeaderOptions/index.js
+++ src/components/HeaderOptions/index.js
@@ -3,9 +3,9 @@ import { Avatar } from "@material-ui/core";
 
 import "./header-options.scss";
 
-const HeaderOptions = ({ avatar, Icon, title }) => {
+const HeaderOptions = ({ avatar, Icon, title, onClick }) => {
   return (
-    <div className="header-options">
+    <div className="header-options" onClick={() => onClick()}>
       {Icon && <Icon className="header-option__icon" />}
       {avatar && <Avatar className="header-option__icon" src={avatar} />}
       <h3 className="header-option__title">{title}</h3>
