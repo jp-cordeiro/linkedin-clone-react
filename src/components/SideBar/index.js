37355c771e56ee305d533083130280030d75009f
JP <jpc.araujo@hotmail.com>
2021-04-05 01:02:32 -0300

    emove static data and add avatar

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,11 +1,16 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
+import { useSelector } from "react-redux";
 import BackgroudnAvatarImage from "../../assets/images/background-avatar.jpg";
+import { avatar } from "../../services/firebase/users";
 
 import "./sidebar.scss";
 
 const SideBar = () => {
   const topics = ["reactjs", "nodejs", "vuejs", "react-native"];
+  const user = useSelector((state) => state.usersReducer.currentUser);
+  console.log(user);
+
   const recentItem = (topic, index) => (
     <div key={index} className="sidebar__recent-item">
       <span className="sidebar__hash">#</span>
@@ -17,9 +22,9 @@ const SideBar = () => {
     <div className="sidebar">
       <img src={BackgroudnAvatarImage} alt="" />
       <div className="sidebar__top">
-        <Avatar className="sidebar__avatar" />
-        <h2>João Paulo Cordeiro</h2>
-        <h4>meuemail@meuemail.com</h4>
+        <Avatar src={avatar(user.uid)} className="sidebar__avatar" />
+        <h2>{user.username}</h2>
+        <h4>{user.email}</h4>
       </div>
 
       <div className="sidebar__stats">
