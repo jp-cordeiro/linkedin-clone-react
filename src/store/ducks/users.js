ba62f12c57ed56fff9fc6bee1a19c6801bd3af73
JP <jpc.araujo@hotmail.com>
2021-04-05 00:59:08 -0300

    setUser

--- src/store/ducks/users.js
+++ src/store/ducks/users.js
@@ -2,6 +2,7 @@ import produce from "immer";
 
 export const Types = {
   FETCH_USER: "users/FETCH_USER",
+  SET_USER: "users/SET_USER",
 };
 
 const INITIAL_STATE = {
@@ -13,6 +14,10 @@ export const usersReducer = produce((state, action) => {
     case "users/FETCH_USER":
       state.currentUser = action.payload;
       break;
+    case "users/SET_USER":
+      console.log(action.payload);
+      state.currentUser = action.payload;
+      break;
     default:
   }
 }, INITIAL_STATE);
@@ -20,6 +25,10 @@ export const usersReducer = produce((state, action) => {
 export const Creators = {
   fetchUser: (user) => ({
     type: Types.FETCH_USER,
-    payload: { user },
+    payload: user,
+  }),
+  setUser: (user) => ({
+    type: Types.SET_USER,
+    payload: user,
   }),
 };
