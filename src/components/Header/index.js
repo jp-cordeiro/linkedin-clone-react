1720856fe15aaba7a9afab96a79b222d13b07b2b
JP <jpc.araujo@hotmail.com>
2021-04-05 01:00:48 -0300

    logout and avatar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useCallback } from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import HomeIcon from "@material-ui/icons/Home";
 import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
 import BusinessCenter from "@material-ui/icons/BusinessCenter";
 import ChatIcon from "@material-ui/icons/Chat";
 import NotificationsIcon from "@material-ui/icons/Notifications";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
 import Logo from "../../assets/images/logo.svg";
+import { auth } from "../../firebase/firebase.utils";
+import { Creators as UserActions } from "../../store/ducks/users";
 
 import "./header.scss";
 import HeaderOptions from "../HeaderOptions";
+import { useDispatch, useSelector } from "react-redux";
+import { avatar } from "../../services/firebase/users";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.usersReducer.currentUser);
   const options = [
     {
       Icon: HomeIcon,
@@ -35,10 +42,14 @@ const Header = () => {
     },
     {
       title: "Eu",
-      avatar:
-        "https://media-exp1.licdn.com/dms/image/C4E03AQGfo-A4O3ELfQ/profile-displayphoto-shrink_100_100/0/1576532321076?e=1622678400&v=beta&t=WBDG2p5ZtiK5Ng7F98PSs13IDDy1WGrQkCJr9gxRGDM",
+      avatar: avatar(user.uid),
     },
   ];
+
+  const logout = useCallback(async () => {
+    dispatch(UserActions.setUser(null));
+    await auth.signOut();
+  }, []);
   return (
     <div className="header">
       <div className="header__left">
@@ -52,6 +63,9 @@ const Header = () => {
         {options.map((option, index) => (
           <HeaderOptions key={index} {...option} />
         ))}
+        {user ? (
+          <HeaderOptions onClick={logout} Icon={ExitToAppIcon} title="Sair" />
+        ) : null}
       </div>
     </div>
   );
