ed9c85f1577ea90df3c675b2633bfa85b7ce8562
JP <jpc.araujo@hotmail.com>
2021-04-05 01:00:11 -0300

    Login

---
+++ src/pages/Login/index.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+import LogoLogin from "../../assets/images/logo-login.svg";
+
+import "./login.scss";
+import SignInForm from "./SignInForm";
+import SignUpForm from "./SingUpForm";
+
+function Login() {
+  const [isSignIn, setIsSignIn] = useState(true);
+  return (
+    <div className="login">
+      <img src={LogoLogin} alt="logo-login" />
+      {isSignIn ? <SignInForm /> : <SignUpForm />}
+      <>
+        {isSignIn ? (
+          <p onClick={() => setIsSignIn(false)}>Ainda não tem um login?</p>
+        ) : (
+          <p onClick={() => setIsSignIn(true)}>Login</p>
+        )}
+      </>
+    </div>
+  );
+}
+
+export default Login;
