98f576bcf469c957cd29e4453fff27945c9a3856
JP <jpc.araujo@hotmail.com>
2021-04-05 01:00:22 -0300

    refactoring App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.scss";
 import Feed from "./components/Feed";
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
+import Login from "./pages/Login";
+
+import { auth } from "./firebase/firebase.utils";
+import { Creators as UserActions } from "./store/ducks/users";
 
 function App() {
+  const user = useSelector((state) => state.usersReducer.currentUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          UserActions.setUser({
+            username: userAuth.displayName,
+            email: userAuth.email,
+            uid: userAuth.uid,
+          })
+        );
+      } else {
+        dispatch(UserActions.setUser(null));
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
-      <Header />
       {/* App Body */}
-      <div className="app__body">
-        <SideBar />
-        <Feed />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className="app__body">
+            <SideBar />
+            <Feed />
+          </div>
+        </>
+      )}
+
       {/* Widgets */}
     </div>
   );
